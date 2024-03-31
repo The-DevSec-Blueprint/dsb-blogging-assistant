df7f8509719202531f9742584eccc7c7c6613485
Damien J. Burks <damienjburks@Damiens-MacBook-Pro.local>
2024-03-31 00:07:16 -0500

    correcting variables - testing

--- variables.tf
+++ variables.tf
@@ -1,23 +1,4 @@
-variable "OPENAI_AUTH_TOKEN" {
-  type        = string
-  description = "API Key for OpenAI's ChatGPT"
-  default     = ""
-}
-
-variable "GIT_USERNAME" {
-  type        = string
-  description = "Username for GitHub account"
-  default     = ""
-}
-
-variable "GIT_AUTH_TOKEN" {
-  type        = string
-  description = "Token for GitHub account"
-  default     = ""
-}
-
-variable "YOUTUBE_AUTH_TOKEN" {
-  type        = string
-  description = "Token for YouTube interaction"
-  default     = ""
-}
+variable "OPENAI_AUTH_TOKEN" {}
+variable "GIT_USERNAME" {}
+variable "GIT_AUTH_TOKEN" {}
+variable "YOUTUBE_AUTH_TOKEN" {}
