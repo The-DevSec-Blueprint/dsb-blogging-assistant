f63564c840d2fbfacd1e5589d3b9b97b44c125ce
Damien J. Burks <damienjburks@Damiens-MacBook-Pro.local>
2024-03-31 00:14:54 -0500

    updating parameter name

--- main.tf
+++ main.tf
@@ -5,25 +5,25 @@ resource "aws_sns_topic" "default" {
 
 # SSM Parameters
 resource "aws_ssm_parameter" "openai_authtoken" {
-  name  = "credentials/openai/auth_token"
+  name  = "/credentials/openai/auth_token"
   type  = "SecureString"
   value = var.OPENAI_AUTH_TOKEN
 }
 
 resource "aws_ssm_parameter" "git_username" {
-  name  = "credentials/git/username"
+  name  = "/credentials/git/username"
   type  = "String"
   value = var.GIT_USERNAME
 }
 
 resource "aws_ssm_parameter" "git_authtoken" {
-  name  = "credentials/git/auth_token"
+  name  = "/credentials/git/auth_token"
   type  = "SecureString"
   value = var.GIT_AUTH_TOKEN
 }
 
 resource "aws_ssm_parameter" "youtube_authtoken" {
-  name  = "credentials/youtube/auth_token"
+  name  = "/credentials/youtube/auth_token"
   type  = "SecureString"
   value = var.YOUTUBE_AUTH_TOKEN
 }
